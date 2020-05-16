f5426cfca1afb4509ab0da1b162bc79495e0014f
Sonny <pet63910@hotmail.fr>
2020-05-16 15:54:08 +0200

    Method map() added in movieList

--- src/App.js
+++ src/App.js
@@ -18,6 +18,11 @@ class App extends Component{
           img:'https://media.senscritique.com/media/000018648166/source_big/Pokemon_le_film_Le_pouvoir_est_en_nous.jpg',
           detail: 'Action, adventure | PG 22',
           description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        },{
+          title: 'LOTR',
+          img:'https://media.senscritique.com/media/000018648166/source_big/Pokemon_le_film_Le_pouvoir_est_en_nous.jpg',
+          detail: 'Action, adventure | PG 22',
+          description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         },
       ],
       selectedMovie:0
--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -5,9 +5,9 @@ export default class MovieList extends Component {
     render() {
         return (
             <div className="w-75 border d-flex flex-row flex-wrap align-content-start pt-4">
-              <MovieElements movies={this.props.movies[0]} updateSelectedMovie={this.props.updateSelectedMovie}/>
-              <MovieElements movies={this.props.movies[1]} updateSelectedMovie={this.props.updateSelectedMovie}/>
-            
+                {this.props.movies.map((m, index)=>(
+                    <MovieElements movies={m} updateSelectedMovie={this.props.updateSelectedMovie}/>
+                ))}
               
             </div>
         )
