4197e17d9865a3e1bcf1646ada2d0c87f0b47d1f
Sonny <pet63910@hotmail.fr>
2020-05-20 11:11:38 +0200

    Change system axios to call API
    With creation folder config (api.movie.js file.) Allow to save the URL and the token (used interceptors)

---
+++ src/conf/api.movie.js
@@ -0,0 +1,12 @@
+import * as axios from 'axios';
+
+const apiMovie = axios.create({
+  baseURL: 'https://api.themoviedb.org/4'
+})
+
+apiMovie.interceptors.request.use(req => {
+  req.headers['Authorization'] = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNmY5MDgyNjhhOTdlODU5OWJlZWEyMDlmMGRjMjkwYyIsInN1YiI6IjVlYzRkZmViOGUyZTAwMDAyMDFjZTZjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2fBELJvLqtiMqOYBc8gov11Bv0-DPLcguQAqEHuqSSA";
+  return req;
+})
+
+export default apiMovie;
